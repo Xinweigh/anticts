cc2f66306b973f7172071a5cb974cc178ce178fc
Xinweigh <zhaizixuan_1@foxmail.com>
2025-09-27 23:42:35 +0800

    完善xinput的支持，支持移动、滚动和按键交互 测试硬件：莱仕达承影
    TODO：移动速度调整为非线性

--- anticts.py
+++ anticts.py
@@ -1,11 +1,12 @@
+import time
 import pygame
-from pynput.mouse import Controller
+from pynput.mouse import Controller, Button
 
 pygame.init()
 clock = pygame.time.Clock()
 pygame.joystick.init()
 mouse = Controller()
-scroll_offset = 0.2
+scroll_offset = 0.8
 move_offset = 35 # 鼠标移动速度修正值，该值越大速度越快
 AX_OFFSET = 1 # 摇杆偏移修正值，该值越大偏移越少，需大于等于1
 
@@ -16,6 +17,12 @@ joystick.init()
 name = joystick.get_name()
 print(f"Joystick name: {name}".format(name))
 
+# 左中右的鼠标按键状态，用来将手柄按键和鼠标映射按键对应，避免刷新过快导致重复按下的情况
+btnflg = [0, 0, 0, 0] 
+
+# B: 1 映射右键, Y: 3 映射中键, X: 2 映射左键
+btn_map = {2:Button.left, 3: Button.middle, 1: Button.right}
+
 while True:
     for event in pygame.event.get(): # User did something
         pass
@@ -23,6 +30,23 @@ while True:
     #         done=True # Flag that we are done so we exit this loop 
 
     axes = joystick.get_numaxes()
+    btns = joystick.get_numbuttons()
+
+    for i in range( btns ):
+        # print(f"{i}, {joystick.get_button(i)}")
+        btnst = joystick.get_button(i)
+
+        # 按键通用模板
+        def check_click(index, btnst):
+            if index in btn_map.keys() and btnst == 1 and btnflg[index] == 0:
+                btnflg[index] = 1
+                mouse.press(btn_map[index])
+            if index in btn_map.keys() and btnst == 0 and btnflg[index] == 1:
+                btnflg[index] = 0
+                mouse.release(btn_map[index])
+
+        check_click(i, btnst)
+
     for i in range( axes ):
         move_dx = 0
         move_dy = 0
@@ -34,22 +58,25 @@ while True:
         #     print(f"Axis {i} value: {axis}")
 
         if i==0: # 0 左侧摇杆左右移动
+            scroll_dx = round(axis, AX_OFFSET) * scroll_offset
+        if i == 1: # 左侧摇杆上下移动
+            scroll_dy = round(axis, AX_OFFSET) * scroll_offset
+        if i == 2: # 右侧摇杆左右移动
             move_dx = round(axis, AX_OFFSET) * move_offset
-        if i == 1 or i == 2: # 1,2 左侧摇杆上下移动
+        if i == 3: # 右侧摇杆上下移动
             move_dy = round(axis, AX_OFFSET) * move_offset
-        if i==3: # 3 右侧摇杆左右移动
-            scroll_dx = -i*round(axis, AX_OFFSET)*scroll_offset
-        if i == 4 or i == 5: # 4,5 右侧摇杆上下移动
-            scroll_dy = -i*round(axis, AX_OFFSET)*scroll_offset
+        if i == 4 : # 左扳机，常态-1, 完全按压为1
+            _ = -i*round(axis, AX_OFFSET)*scroll_offset
+        if i == 5 : # 右扳机，常态-1, 完全按压为1
+            _ = -i*round(axis, AX_OFFSET)*scroll_offset
 
-        # Axis 0: -1 left, 1 right
-        # Axis 1: -1 up, 1 down
-        # Axis 2: -1 up, 1 down
-        # Axis 3: -1 left, 1 right
-        # mouse scroll dy: -1 down, 1 up
-        mouse.scroll(scroll_dx, scroll_dy)
+
+        if scroll_dx != 0 or scroll_dy != 0:
+            if abs(scroll_dx) > abs(scroll_dy):
+                mouse.scroll(scroll_dx, 0)
+            else:
+                mouse.scroll(0, scroll_dy)
         if move_dy != 0 or move_dx != 0:
-            # print(f"move_dx: {move_dx}, move_dy: {move_dy}")
             mouse.move(move_dx, move_dy)
         
 
